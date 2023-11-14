38c66518bb4c30279fdfae71a7581b1a5e602451
Ryan Blake <ryan.blake@gmail.com>
2023-11-14 14:03:43 -0500

    Adding SEO Plugin

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "$fresh/server.ts";
+import { freshSEOPlugin } from "https://deno.land/x/fresh_seo@1.0.1/mod.ts";
+import manifest from "./fresh.gen.ts";
 import twindPlugin from "twind_fresh_plugin/twind.ts";
 import twindConfig from "./twind.config.ts";
 
 export default defineConfig({
-	plugins: [twindPlugin(twindConfig)],
+	plugins: [twindPlugin(twindConfig), freshSEOPlugin(manifest)],
 });
