45dbc626cc405290c39d503f5c20df3231e80fc1
Ryan Blake <ryan.blake@gmail.com>
2023-11-14 13:07:15 -0500

    Getting nav bills pagination/nav bar to behave a little better while deno is doing its magic

--- islands/BillsNav.tsx
+++ islands/BillsNav.tsx
@@ -60,36 +60,37 @@ export default ({ nextPage, previousPage, offsetUnsafe, pageSize }: Props) => {
 					))}
 				</Select>
 			</span>
-			{nextPage && (
-				<a
-					id="nextPage"
-					title="Next Page"
-					onClick={(e) =>
-						onEvent(e, () => {
-							nextPage &&
-								(offsetUnsafe.value = new URL(nextPage).searchParams.get(
-									"offset",
-								));
-						})}
-					class="cursor-pointer hover:underline col-start-3 justify-self-end"
+			<a
+				id="nextPage"
+				title="Next Page"
+				onClick={(e) =>
+					onEvent(e, () => {
+						nextPage &&
+							(offsetUnsafe.value = new URL(nextPage).searchParams.get(
+								"offset",
+							));
+					})}
+				disabled={!nextPage}
+				class={cx("cursor-pointer hover:underline col-start-3 justify-self-end", {
+					"collapse": !nextPage,
+				})}
+			>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="#000000"
+					height="25px"
+					width="25px"
+					version="1.1"
+					id="right-arrow"
+					viewBox="0 0 330.002 330.002"
+					xml:space="preserve"
 				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="#000000"
-						height="25px"
-						width="25px"
-						version="1.1"
-						id="right-arrow"
-						viewBox="0 0 330.002 330.002"
-						xml:space="preserve"
-					>
-						<path
-							id="XMLID_103_"
-							d="M233.252,155.997L120.752,6.001c-4.972-6.628-14.372-7.97-21-3c-6.628,4.971-7.971,14.373-3,21  l105.75,140.997L96.752,306.001c-4.971,6.627-3.627,16.03,3,21c2.698,2.024,5.856,3.001,8.988,3.001  c4.561,0,9.065-2.072,12.012-6.001l112.5-150.004C237.252,168.664,237.252,161.33,233.252,155.997z"
-						/>
-					</svg>
-				</a>
-			)}
+					<path
+						id="XMLID_103_"
+						d="M233.252,155.997L120.752,6.001c-4.972-6.628-14.372-7.97-21-3c-6.628,4.971-7.971,14.373-3,21  l105.75,140.997L96.752,306.001c-4.971,6.627-3.627,16.03,3,21c2.698,2.024,5.856,3.001,8.988,3.001  c4.561,0,9.065-2.072,12.012-6.001l112.5-150.004C237.252,168.664,237.252,161.33,233.252,155.997z"
+					/>
+				</svg>
+			</a>
 		</span>
 	);
 };
