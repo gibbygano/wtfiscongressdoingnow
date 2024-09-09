64fd457a2099093b3fdd7662d4d1b3d35219f951
Ryan Blake <ryan.blake@gmail.com>
2024-09-09 04:30:19 -0400

    Redoing grid

--- islands/BlueSky.tsx
+++ islands/BlueSky.tsx
@@ -1,7 +1,7 @@
 export default ({ feed }: { feed: Array<string> }) => {
     console.log(feed);
     return (
-        <div class="lg:columns-3 2xl:columns-5 xl:columns-4 md:columns-2 sm:columns-1 md:text-sm lg:text-base text-xs gap-5 px-7 pt-10">
+        <div class="2xl:columns-3 xl:columns-2 sm:columns-1 md:text-sm lg:text-base text-xs gap-5 px-7 pt-10">
             {feed.map((li) => <div dangerouslySetInnerHTML={{ __html: li }} />)}
             <script async src="https://embed.bsky.app/static/embed.js" charset="utf-8"></script>
         </div>
