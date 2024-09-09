782dd910e97eb92a6e38409811b3dc7b49cbc82b
Ryan Blake <ryan.blake@gmail.com>
2024-09-08 22:40:08 -0400

    BlueSky embed

---
+++ routes/bsky.tsx
@@ -0,0 +1,22 @@
+export default () => (
+    <div class="h-screen flex justify-center mt-10">
+        <blockquote
+            class="bluesky-embed"
+            data-bluesky-uri="at://did:plc:ru4uwws7zdxrncwr2a4jhask/app.bsky.feed.post/3l3orwsydyc24"
+            data-bluesky-cid="bafyreiagqlqhq5e7nydut4srwq6jghguyfk7ok4toshutdnurt2ckq5pza"
+        >
+            <p lang="en">
+                @bsky.app Making a grumpy face because even though every DNS server I checked showed
+                the proper TXT DNS record and the verify worked but I kept getting &quot;External
+                handle did not resolve to DID&quot;. Now I&#x27;m rate limited 😠
+            </p>&mdash; Silladelphia
+            (<a href="https://bsky.app/profile/did:plc:ru4uwws7zdxrncwr2a4jhask?ref_src=embed">
+                @silladelphia.wtfiscongressdoingnow.us
+            </a>){" "}
+            <a href="https://bsky.app/profile/did:plc:ru4uwws7zdxrncwr2a4jhask/post/3l3orwsydyc24?ref_src=embed">
+                Sep 8, 2024 at 9:50 PM
+            </a>
+        </blockquote>
+        <script async src="https://embed.bsky.app/static/embed.js" charset="utf-8"></script>
+    </div>
+);
