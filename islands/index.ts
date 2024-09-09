490177f39a8342f72f5b0e72ddd25ed747b56b22
Ryan Blake <ryan.blake@gmail.com>
2024-09-09 05:58:33 -0400

    Update index.ts

--- islands/index.ts
+++ islands/index.ts
@@ -1,6 +1,5 @@
 import Bills from "./Bills.tsx";
 import BillsNav from "./BillsNav.tsx";
 import BillSummaryAccordion from "./BillSummaryAccordion.tsx";
-import BlueSky from "./BlueSky.tsx";
 
-export { Bills, BillsNav, BillSummaryAccordion, BlueSky };
+export { Bills, BillsNav, BillSummaryAccordion };
