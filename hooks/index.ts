0d09d07e822e7f1bb9862637583cc8409ec00458
Ryan Blake <ryan.blake@gmail.com>
2024-11-17 17:20:55 -0500

    Giving up on the PWA mess

--- hooks/index.ts
+++ hooks/index.ts
@@ -2,12 +2,5 @@ import useDownload from "./useDownload.ts";
 import useFetchActions from "./useFetchActions.ts";
 import useFetchBills from "./useFetchBills.ts";
 import useFetchBillSummary from "./useFetchBillSummary.ts";
-import useRegisterServiceWorker from "./useRegisterServiceWorker.ts";
 
-export {
-	useDownload,
-	useFetchActions,
-	useFetchBills,
-	useFetchBillSummary,
-	useRegisterServiceWorker,
-};
+export { useDownload, useFetchActions, useFetchBills, useFetchBillSummary };
