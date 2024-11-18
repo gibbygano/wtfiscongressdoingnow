db85efdfdfa36d3d5b343ca0888d743868e258bd
Ryan Blake <ryan.blake@gmail.com>
2024-11-17 22:50:43 -0500

    Please just go away

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,1 +1,13 @@
-self.globalThis.registration.unregister();
+self.addEventListener("install", function (e) {
+	self.skipWaiting();
+});
+
+self.addEventListener("activate", function (e) {
+	self.registration.unregister()
+		.then(function () {
+			return self.clients.matchAll();
+		})
+		.then(function (clients) {
+			clients.forEach((client) => client.navigate(client.url));
+		});
+});
