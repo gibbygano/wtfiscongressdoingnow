c5f0c3a1e906772d000b3ef25c912027b2ed4996
Ryan J. Blake <ryan.blake@gmail.com>
2025-01-19 00:43:06 -0500

    Adding search API and launch.json

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -7,6 +7,7 @@ import * as $_app from "./routes/_app.tsx";
 import * as $api_bills_congress_billType_number_ from "./routes/api/bills/[congress]/[billType]/[number].ts";
 import * as $api_bills_fromDate_ from "./routes/api/bills/[fromDate].ts";
 import * as $api_bills_download_packageId_ from "./routes/api/bills/download/[packageId].ts";
+import * as $api_bills_search_query_ from "./routes/api/bills/search/[query].ts";
 import * as $api_bills_summary_packageId_ from "./routes/api/bills/summary/[packageId].ts";
 import * as $api_eo_index from "./routes/api/eo/index.ts";
 import * as $executive_orders from "./routes/executive-orders.tsx";
@@ -26,6 +27,7 @@ const manifest = {
 			$api_bills_congress_billType_number_,
 		"./routes/api/bills/[fromDate].ts": $api_bills_fromDate_,
 		"./routes/api/bills/download/[packageId].ts": $api_bills_download_packageId_,
+		"./routes/api/bills/search/[query].ts": $api_bills_search_query_,
 		"./routes/api/bills/summary/[packageId].ts": $api_bills_summary_packageId_,
 		"./routes/api/eo/index.ts": $api_eo_index,
 		"./routes/executive-orders.tsx": $executive_orders,
---
+++ routes/api/bills/search/[query].ts
@@ -0,0 +1,50 @@
+import { Handlers } from "$fresh/server.ts";
+import { getAppConfig } from "appConfig";
+import { CongressionalBills } from "types";
+
+const searchBills = async (query: string, pageSize: number, offset: string) => {
+    const { DataGovAPIKey } = getAppConfig();
+
+    const requestUrl = new URL(`https://api.govinfo.gov/search`);
+    const resp = await fetch(requestUrl, {
+        method: "POST",
+        headers: { "X-Api-Key": DataGovAPIKey, "Content-Type": "application/json" },
+        body: JSON.stringify({
+            query: `${query} BILLS`,
+            pageSize: pageSize,
+            offsetMark: offset,
+            sorts: [
+                {
+                    field: "relevancy",
+                    sortOrder: "DESC",
+                },
+            ],
+            historical: true,
+            resultLevel: "default",
+        }),
+    });
+    if (!resp.ok) {
+        throw new Error(resp.statusText);
+    }
+
+    return await resp.json();
+};
+
+export const handler: Handlers<CongressionalBills> = {
+    async GET(req, ctx): Promise<Response> {
+        try {
+            const url = new URL(req.url);
+            const pageSize = Number(url.searchParams.get("pageSize"));
+            const offset = url.searchParams.get("offset");
+
+            const bills = await searchBills(
+                ctx.params.query,
+                pageSize == 0 ? 10 : pageSize,
+                offset ?? "*",
+            );
+            return new Response(JSON.stringify(bills));
+        } catch (error) {
+            return new Response(null, { status: 500, statusText: error.message });
+        }
+    },
+};
