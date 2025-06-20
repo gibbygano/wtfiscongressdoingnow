c165db068aefc78901af7227e1255b716e9f9511
Ryan J. Blake <ryan.blake@gmail.com>
2025-06-20 00:09:28 -0400

    dropping 'Head' and removing base layer from stylesheet

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,10 +1,10 @@
 import { PageProps } from "$fresh/server.ts";
-import { Head } from "$fresh/runtime.ts";
+import { asset } from "$fresh/runtime.ts";
 import { Footer, Header } from "components";
 
 const App = ({ Component }: PageProps) => (
 	<html lang="en">
-		<Head>
+		<head>
 			<meta charset="utf-8" />
 			<meta
 				name="viewport"
@@ -15,11 +15,12 @@ const App = ({ Component }: PageProps) => (
 			<link rel="preconnect" href="https://fonts.googleapis.com" />
 			<link rel="preconnect" href="https://fonts.gstatic.com" />
 			<link
+				defer
 				rel="stylesheet"
 				href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
 			/>
-			<link rel="stylesheet" href="/styles.css" />
-		</Head>
+			<link defer rel="stylesheet" href={asset("/styles.css")} />
+		</head>
 		<body class="min-h-screen flex flex-col">
 			<Header />
 			<main class="w-full h-full flex flex-col flex-grow bg-gradient-to-tr from-red-200 via-slate-100 to-blue-200">
