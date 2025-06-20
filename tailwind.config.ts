800465c10308a15e2a2f0179f8637d7ac661c6fd
Ryan J. Blake <ryan.blake@gmail.com>
2025-06-20 00:48:30 -0400

    Had to change typography import to npm:*

--- tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "tailwindcss";
-import forms from "@tailwindcss/forms";
-
-const tailwindsConfig: Config = {
-	content: ["{routes,islands,components}/**/*.{ts,tsx}"],
-	plugins: [forms],
-};
-
-export default tailwindsConfig;
