9a04b4dc64fbc6f10e313c23d62a937575ef7621
Ryan J. Blake <ryan.blake@gmail.com>
2025-06-20 01:27:15 -0400

    preload and fetchpriority on header img

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -15,6 +15,13 @@ const App = ({ Component }: PageProps) => (
 			<link rel="preconnect" href="https://fonts.googleapis.com" />
 			<link rel="preconnect" href="https://fonts.gstatic.com" />
 			<link
+				rel="preload"
+				as="image"
+				// @ts-ignore - fetchpriority is valid
+				fetchpriority="high"
+				href={asset("/images/congress.jpg")}
+			/>
+			<link
 				defer
 				rel="stylesheet"
 				href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
