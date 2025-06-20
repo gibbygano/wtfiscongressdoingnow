f1a1abb8f3548a834f9d3fdacab56709a2cbf123
Ryan J. Blake <ryan.blake@gmail.com>
2025-06-19 23:56:44 -0400

    Missing styles

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -2,35 +2,33 @@ import { PageProps } from "$fresh/server.ts";
 import { asset, Head } from "$fresh/runtime.ts";
 import { Footer, Header } from "components";
 
-export default function App({ Component }: PageProps) {
-	const styles = asset("/styles.css");
+const App = ({ Component }: PageProps) => (
+	<html lang="en">
+		<Head>
+			<meta charset="utf-8" />
+			<meta
+				name="viewport"
+				content="width=device-width, initial-scale=1.0"
+			/>
+			<meta name="description" content="List of congressional bills by date"></meta>
+			<title>WTF Is Congress Doing Now?</title>
+			<link rel="preconnect" href="https://fonts.googleapis.com" />
+			<link rel="preconnect" href="https://fonts.gstatic.com" />
+			<link
+				defer
+				rel="stylesheet"
+				href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+			/>
+			<link defer rel="stylesheet" href={asset("/styles.css")} />
+		</Head>
+		<body class="min-h-screen flex flex-col">
+			<Header />
+			<main class="w-full h-full flex flex-col flex-grow bg-gradient-to-tr from-red-200 via-slate-100 to-blue-200">
+				<Component />
+			</main>
+			<Footer />
+		</body>
+	</html>
+);
 
-	return (
-		<html lang="en">
-			<Head>
-				<meta charset="utf-8" />
-				<meta
-					name="viewport"
-					content="width=device-width, initial-scale=1.0"
-				/>
-				<meta name="description" content="List of congressional bills by date"></meta>
-				<title>WTF Is Congress Doing Now?</title>
-				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link rel="preconnect" href="https://fonts.gstatic.com" />
-				<link
-					defer
-					rel="stylesheet"
-					href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
-				/>
-				<link async rel="stylesheet" href={styles} />
-			</Head>
-			<body class="min-h-screen flex flex-col">
-				<Header />
-				<main class="w-full h-full flex flex-col flex-grow bg-gradient-to-tr from-red-200 via-slate-100 to-blue-200">
-					<Component />
-				</main>
-				<Footer />
-			</body>
-		</html>
-	);
-}
+export default App;
