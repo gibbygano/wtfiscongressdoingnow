2ca148a82936e73b5ee3746d951eaaead7257880
Ryan J. Blake <ryan.blake@gmail.com>
2025-06-20 01:30:49 -0400

    Putting 'Head' back

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,10 +1,10 @@
 import { PageProps } from "$fresh/server.ts";
-import { asset } from "$fresh/runtime.ts";
+import { asset, Head } from "$fresh/runtime.ts";
 import { Footer, Header } from "components";
 
 const App = ({ Component }: PageProps) => (
 	<html lang="en">
-		<head>
+		<Head>
 			<meta charset="utf-8" />
 			<meta
 				name="viewport"
@@ -27,7 +27,7 @@ const App = ({ Component }: PageProps) => (
 				href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
 			/>
 			<link defer rel="stylesheet" href={asset("/styles.css")} />
-		</head>
+		</Head>
 		<body class="min-h-screen flex flex-col">
 			<Header />
 			<main class="w-full h-full flex flex-col flex-grow bg-gradient-to-tr from-red-200 via-slate-100 to-blue-200">
