7effcb625f6519ce17d24ccb97f2fb618da313d9
Ryan Blake <ryan.blake@gmail.com>
2025-09-21 20:40:15 -0400

    implemented daisyui card

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -13,15 +13,17 @@ export default (
 		actionChildren,
 	}: Props,
 ) => (
-	<div class="flex flex-col bg-white border rounded-xl dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]n shadow-xl break-inside-avoid mb-5 bottom-0">
-		<div class="p-4 md:p-5 prose leading-6">
-			<h3 class="text-lg font-semibold text-gray-800 dark:text-white">
-				{headerText}
-			</h3>
-			{children}
-		</div>
-		<div class="py-4 md:py-5 md:px-10 px-5">
-			{actionChildren}
+	<div class="flex flex-col card card-border bg-white border rounded-xl dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]n shadow-xl break-inside-avoid mb-5 bottom-0">
+		<div class="card-body">
+			<div class="p-4 md:p-5 prose leading-6">
+				<h3 class="text-lg card-title font-semibold text-gray-800 dark:text-white">
+					{headerText}
+				</h3>
+				{children}
+			</div>
+			<div class="py-4 card-body md:py-5 md:px-10 px-5">
+				{actionChildren}
+			</div>
 		</div>
 	</div>
 );
