bf0bdc6bc9a876dfd2c0da774dd88f8d6949c941
Ryan J. Blake <ryan.blake@gmail.com>
2026-01-06 16:11:04 -0500

    using masonry (but deprecated since chrome doesnt' support the new shidd)

--- components/shared/Grid.tsx
+++ components/shared/Grid.tsx
@@ -9,7 +9,7 @@ const Grid = ({ children, id }: CardGridProps) => {
   return (
     <div class="mb-44 flex-1">
       <div
-        class="lg:columns-3 2xl:columns-4 3xl:ml-96 3xl:mr-96 md:columns-2 sm:columns-1 md:text-sm lg:text-base text-xs gap-5 px-7 md:pt-16 pt-10"
+        class="masonry lg:grid-cols-3 2xl:grid-cols-4 3xl:ml-96 3xl:mr-96 md:grid-cols-2 sm:grid-cols-1 md:text-sm lg:text-base text-xs gap-5 px-7 md:pt-16 pt-10"
         id={id}
       >
         {children}
